9debc69dbfa3a7740493fb6695e9901d4f16c5b7
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-03-15 08:08:14 -0300

    01-cap01-completo

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: [
+          Text(
+            'Bem vindo ao Bytebank',
+            textDirection: TextDirection.ltr,
+          ), Text(
+            'Bem vindo ao Bytebank',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: [
+              Text(
+                'Bem vindo ao Bytebank',
+                textDirection: TextDirection.ltr,
+              ), Text(
+                'Bem vindo ao Bytebank',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
